7e7ca11c6192a2993755498f6eca6af51f2eb8ac
Alex Shvets <dev@sashok.mk.ua>
2013-08-10 03:34:59 +0300

    add jsdoc

--- nano-clone.js
+++ nano-clone.js
@@ -1,4 +1,7 @@
-function clone(/** Object */proto, /** ObjectLiteralOnly */properdies){
+/** Creates clone of proto.
+ * @see www.github.com/quadroid/nano-clone */
+function clone(/** Object */proto, /** object.literalOnly! */properdies){
     properdies.__proto__ = proto;
     return properdies;
 }
+/** @typedef {Object} object.literalOnly */
